d6ace5795c81d345932ba1eea537891744f0d25c
dkovacshw <kovacsdavid.uni@gmail.com>
2026-02-26 18:57:43 +0100

    Update ascii_tabla.c

--- week2/ascii_tabla.c
+++ week2/ascii_tabla.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int main() {
-
+void print_ascii() {
     for (int i = 32; i <= 126; i++) {
         printf("%d: %c\n", i, i);
     }
+}
 
+int sum_uppercase() {
+    int sum = 0;
+    for (int i = 65; i <= 90; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main() {
+    print_ascii();
+    printf("\nSum of ASCII values (A-Z): %d\n", sum_uppercase());
     return 0;
 }
