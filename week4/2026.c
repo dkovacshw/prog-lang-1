b610c680fb3a37fb57eb11b93b023c5cdff9e440
dkovacshw <kovacsdavid.uni@gmail.com>
2026-04-07 12:21:48 +0200

    Create 2026.c

---
+++ week4/2026.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    putchar('a' - '/');
+    putchar('p' - '@');
+    putchar('a' - '/');
+    putchar('x' - 'B');
+    putchar('\n');
+    return EOF - EOF;
+}
